3dc5b51497f161ef006902535f393a3fd91cc43a
Priyanshu Kumar <krpriyanshu79858@gmail.com>
2025-09-30 23:25:24 +0530

    fixed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppWrapper from './App.jsx'; // 1. Import AppWrapper, NOT App
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppWrapper /> {/* 2. Render AppWrapper here */}
+  </React.StrictMode>,
+);
