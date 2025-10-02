e9d98df8a55ae07ccb560d426d6460eb034e4601
Priyanshu Kumar <krpriyanshu79858@gmail.com>
2025-10-02 18:15:37 +0530

    fixed issue

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = "https://alumniversebackend.onrender.com/api";// Your backend server URL
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://alumniversebackend.onrender.com/api";// Your backend server URL
+// const BASE_URL = "http://localhost:5000/api";
 export {BASE_URL};
 
 console.log(BASE_URL);
